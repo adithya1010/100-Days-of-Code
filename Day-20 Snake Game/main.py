d881cadba99e7ff23108719924eb80c9263adf27
Adithya S.T <adist1340@gmail.com>
2022-08-31 13:29:53 +0530

    Day-20-Snake Game-Part-I-Added Comments

--- Day-20 Snake Game/main.py
+++ Day-20 Snake Game/main.py
@@ -10,17 +10,16 @@ screen.title("My Snake Game")
 
 snake = Snake()
 
-screen.listen()
-screen.onkey(key="Up", fun=snake.up)
-screen.onkey(key="Down", fun=snake.down)
-screen.onkey(key="Left", fun=snake.left)
-screen.onkey(key="Right", fun=snake.right)
+screen.listen()  # Listening to Keystrokes
+screen.onkey(key="Up", fun=snake.up)  # Binding Up key to up function in snake.py
+screen.onkey(key="Down", fun=snake.down)  # Binding Down key to up function in snake.py
+screen.onkey(key="Left", fun=snake.left)  # Binding Left key to up function in snake.py
+screen.onkey(key="Right", fun=snake.right)  # Binding Right key to up function in snake.py
 
 game_is_on = True
 while game_is_on:
     screen.update()  # Updating the Screen
     time.sleep(0.1)  # delaying the snake by 0.1 seconds
-    snake.move()
-
+    snake.move()  # Calling the move function in snake.py to move the snake
 
 screen.exitonclick()
--- Day-20 Snake Game/snake.py
+++ Day-20 Snake Game/snake.py
@@ -1,9 +1,11 @@
 from turtle import Turtle, Screen
 import time
 
+# Declaring constants for the starting position and the move distance for the snake
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 
+# Declaring constants of the directions of the snake
 UP = 90
 DOWN = 270
 LEFT = 180
@@ -17,6 +19,7 @@ class Snake:
         self.create_snake()
         self.head = self.segments[0]
 
+    # Creating the snake
     def create_snake(self):
         for position in STARTING_POSITIONS:
             new_segment = Turtle("square")
@@ -25,6 +28,7 @@ class Snake:
             new_segment.goto(position)
             self.segments.append(new_segment)
 
+    # Moving the snake
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
@@ -32,18 +36,22 @@ class Snake:
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
 
+    # Function for moving the snake up
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
 
+    # Function for moving the snake down
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
+    # Function for moving the snake left
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
 
+    # Function for moving the snake down
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
